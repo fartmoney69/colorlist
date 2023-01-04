eedfcc73d5f3ab10cebed2ceaf41cf01eb363c46
fartmoney69 <116140776+fartmoney69@users.noreply.github.com>
2023-01-04 12:11:53 +0700

    Add files via upload

---
+++ traverselist.py
@@ -0,0 +1,8 @@
+colorslist = []
+repeat = "Y"
+while repeat == "Y":
+    colors = input("enter a color: ")
+    colorslist.append(name)
+    repeat =  input("Do you want to add another color?(Y/N) ")
+for i in range(len(colorlist)):
+    print(i, colorslist[i])
